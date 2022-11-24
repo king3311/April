1a71042f1ee6af3144b14d139cbc775cc3c35270
king3311 <d76493315@gmail.com>
2022-11-24 21:16:23 +0530

    Newly push

---
+++ src/org/project112/Project112.java
@@ -0,0 +1,13 @@
+package org.project112;
+
+public class Project112 {
+
+	private void project11() {
+	System.out.println("11");	
+	}
+	
+	private void project22() {
+		System.out.println("22");
+	}
+	
+}
