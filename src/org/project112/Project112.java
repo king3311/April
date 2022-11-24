0c5d7c56ca1304b73af6b37658aaae97faaca818
king3311 <d76493315@gmail.com>
2022-11-24 21:27:47 +0530

    modified

--- src/org/project112/Project112.java
+++ src/org/project112/Project112.java
@@ -9,5 +9,16 @@ public class Project112 {
 	private void project22() {
 		System.out.println("22");
 	}
+	private void project33() {
+	  System.out.println("33");
+
+	}
+	
+	private void project44() {
+		  System.out.println("44");
+
+		}
+	
+	
 	
 }
